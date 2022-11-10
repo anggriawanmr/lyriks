4834641eb9d7133cffbbf597df9a18693e1aa6d5
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-11 02:39:38 +0700

    Adding SongCard component :sparkles:

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import playerReducer from './features/playerSlice';
 import { shazamCoreApi } from './services/shazamCore';
+import playerReducer from './features/playerSlice';
 
 export const store = configureStore({
   reducer: {
-    [shazamCoreApi.reducerAPI]: shazamCoreApi.reducer,
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
   middleware: (getDefaultMiddleware) =>
