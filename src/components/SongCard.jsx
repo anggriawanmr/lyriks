62f979dcd3cd9514335350130e091ecbf0aa6270
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-11 04:09:39 +0700

    Adding PlayPause component :sparkles:

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,10 +4,17 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, isPlaying, activeSong, i }) => {
-  const handlePauseClick = () => {};
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
