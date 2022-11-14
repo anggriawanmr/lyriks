f8df7f80eb4e671c9d7f95b842149028fe772814
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-14 16:32:34 +0700

    Adding SongDetails component :sparkles:

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,19 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  return (
+    <div className="flex flex-col ">
+      <DetailsHeader artistid={artistid} songData={songData} />
+    </div>
+  );
+};
 
 export default SongDetails;
