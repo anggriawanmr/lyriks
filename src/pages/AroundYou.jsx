00d2c099dd0ca607c18c5ada5516da7bdbd99331
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-16 23:30:27 +0700

    Adding AroundYou component :sparkles:

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { setActiveSong } from '../redux/features/playerSlice';
 
-export default CountryTracks;
+const AroundYou = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=at_mg4tMyQr38o4vEpexhUiuZV1YTDxg`
+      )
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  return <div>AroundYou</div>;
+};
+
+export default AroundYou;
