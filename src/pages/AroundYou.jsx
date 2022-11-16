9b865bb7b9caba58b3d32c540396eb9a8bc3f47b
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-16 23:48:27 +0700

    Editing AroundYou component :hammer:

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -3,14 +3,13 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
-import { setActiveSong } from '../redux/features/playerSlice';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const AroundYou = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
-  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
-
-  console.log(country);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios
@@ -22,7 +21,31 @@ const AroundYou = () => {
       .finally(() => setLoading(false));
   }, [country]);
 
-  return <div>AroundYou</div>;
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white mt-4 text-left mb-10">
+        Around You
+        <span className="font-black"> {country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AroundYou;
