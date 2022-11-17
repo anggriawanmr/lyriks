46c969d0800cb6c0bcadeef4f3c75db186826c75
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-17 15:11:21 +0700

    Editing genre drop-down feature :hammer:

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -15,6 +15,9 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
     getSongDetails: builder.query({
       query: ({ songid }) => `/tracks/details?track_id=${songid}`,
     }),
@@ -36,4 +39,5 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
 } = shazamCoreApi;
